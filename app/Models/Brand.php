002c2613f7d5ad54d1d583f24bdd36844a95821d
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-03-19 08:56:37 +0100

    llenado tabla brands y uso de metodo "attach()"

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Brand extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+
+    //n:n entre brands y categories
+    public function categories()
+    {
+        return $this->BelongsToMany(Category::class);
+    }
+
+    //1:n entre brands y products
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -18,37 +19,40 @@ class CategorySeeder extends Seeder
         $categories = [
             [
                 'name' => 'Celulares y tablets',
-                'slug' => Str::slug('Celulares y tablets') , //la clase Str me permite usar slug
+                'slug' => Str::slug('Celulares y tablets'), //la clase Str me permite usar slug
                 'icon' => '<i class="fa-solid fa-mobile-screen"></i>'
             ],
             [
                 'name' => 'TV, audio y video',
-                'slug' => Str::slug('TV, audio y video') , //la clase Str me permite usar slug
+                'slug' => Str::slug('TV, audio y video'), //la clase Str me permite usar slug
                 'icon' => '<i class="fa-solid fa-tv-music"></i>'
             ],
             [
                 'name' => 'Consola y videojuegos',
-                'slug' => Str::slug('Consola y videojuegos') , //la clase Str me permite usar slug
+                'slug' => Str::slug('Consola y videojuegos'), //la clase Str me permite usar slug
                 'icon' => '<i class="fa-solid fa-gamepad-modern"></i>'
             ],
             [
                 'name' => 'Computacion',
-                'slug' => Str::slug('Computacion') , //la clase Str me permite usar slug
+                'slug' => Str::slug('Computacion'), //la clase Str me permite usar slug
                 'icon' => '<i class="fa-solid fa-laptop"></i>'
             ],
             [
                 'name' => 'Moda',
-                'slug' => Str::slug('Moda') , //la clase Str me permite usar slug
+                'slug' => Str::slug('Moda'), //la clase Str me permite usar slug
                 'icon' => '<i class="fa-solid fa-shirt"></i>'
             ],
         ];
 
 
-        foreach($categories as $category){
-            Category::factory(1)->create($category);
+        foreach ($categories as $category) {
+            $category = Category::factory(1)->create($category)->first();
+
+            $brands = Brand::factory(4)->create();
+
+            foreach ($brands as $brand) {
+                $brand->categories()->attach($category->id); //el metodo attach introduce registros en la tabla intermedia
+            }
         }
-
-
-
     }
 }
