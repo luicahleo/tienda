d194c523eb9901278059d49c149b472d06761f1d
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-03-20 08:16:35 +0100

    diseñando header, publicando compone jetstream, flex-1 y w-full

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,21 @@
-<header class="container">
-    <div>
-        Hola Mundo
+<header class="bg-trueGray-700   ">
+    <div class="container flex items-center h-16 ">
+        <a href=""
+            class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <span>Categorias</span>
+        </a>
+        {{-- no olvidarme, para usar componentes jetstream, tengo que publicarlos con php artisan vendor:publish --tag=jetstream-views --}}
+        {{-- los componentes ahora estana en, resources/views/vendor\jetstream --}}
+
+        <a href="/" class="mx-6">
+            <x-jet-application-mark class="block h-9 w-auto" />
+
+        </a>
+
+        @livewire('search')
     </div>
 </header>
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,5 @@
+<div class="flex-1">
+
+    <x-jet-input type="text" class="w-full" placeholder="¿Estas buscando algun producto?"  />
+
+</div>
