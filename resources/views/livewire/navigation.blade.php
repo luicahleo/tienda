df8b345a3657838c5a42fe799f2adeb3070087b9
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-04-01 19:13:19 +0200

    diseñ submenu 2, he usado componentes para reemplazar el submenu, paso de objeto a componente con el :

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -3,6 +3,10 @@
         height: calc(100vh - 4rem);
     }
 
+    .navigation-link:hover .navigation-submenu {
+        display: block !important;
+    }
+
 </style>
 
 
@@ -94,7 +98,7 @@
             <div class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach ($categories as $category)
-                        <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                        <li class=" navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
                             <a href="" class="py-2 px-4 text-sm flex items-center">
                                 <span class="flex justify-center w-9">
                                     {{-- {!! xxxx !!} con  esto escapamos el codigo html, es decir para que nos imprima el codigo html y no lo imprima como texto plano --}}
@@ -103,7 +107,10 @@
                                 {{ $category->name }}
                             </a>
 
-                            <div class=" bg-red-500 absolute w-3/4 h-full top-0 right-0 hidden">
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                                {{-- llamamos al componente para mostrar category --}}
+                                <x-navigation-subcategories :category="$category" />
+
                             </div>
                         </li>
                     @endforeach
@@ -111,27 +118,9 @@
                 </ul>
                 <div class="col-span-3 bg-gray-100">
 
-                    <div class="grid grid-cols-4 p-4">
-                        <div>
-                            <p class="text-lg font-bold text-center text-trueGray-500 mb-3">Subcategorias</p>
-
-                            <ul>
-                                @foreach ($categories->first()->subcategories as $subcategory)
-                                    <li>
-                                        <a href=""
-                                            class="text-trueGray-500 inline-block font-semibold py-1 px-4 hover:text-orange-500">
-                                            {{ $subcategory->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <div class="col-span-3">
-                            <img class="h-64 w-full object-cover object-center"
-                                src="{{ Storage::url($categories->first()->image) }}" alt="">
-                        </div>
-
-                    </div>
+                    {{-- llamamos al componente para mostrar subcategorias --}}
+                    {{-- para pasar un objeto al componente usamos :    osea :category="el objeto" --}}
+                    <x-navigation-subcategories :category="$categories->first()" />
 
                 </div>
 
