58c72c53ee4d02313b48c81a702857c52866d81c
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-04-02 00:01:08 +0200

    funcionalidad al menu, pero con errores, aqui se empieza usar alpine

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,5 +38,32 @@
         @stack('modals')
 
         @livewireScripts
+
+        <script>
+            function dropdown() {
+                return {
+                    open: false,
+                    show(){
+                        if (this.open) {
+                            //se cierra el menu
+                            this.open = false;
+                            //esto es para el scroll
+                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
+        
+                        } else {
+                            //se abre el menu
+                            this.open = true;
+                            document.getElementsByTagName('html')[0].style.overflow = 'hidden'
+                        },
+                        close() {
+                            //se cierra el menu
+                            this.open = false;
+                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                        }
+                    }
+                }
+            }
+        </script>
+        
     </body>
 </html>
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,23 +1,14 @@
-<style>
-    #navigation-menu {
-        height: calc(100vh - 4rem);
-    }
-
-    .navigation-link:hover .navigation-submenu {
-        display: block !important;
-    }
-
-</style>
-
-
 {{-- sticky top-0  esto es para que el menu se quede pegado en la parte de arriba y al hacer scroll no baje con nostros --}}
-<header class="bg-trueGray-700 sticky top-0  ">
+{{-- voy a usar Alpine porque ya viene instalado con jetstream, y cuando uso x-data, alpine considera que solo trabajara 
+    en esa parte usandolo como componente, pudiendo asi usar las variables en otro lugar donde defina x-data --}}
+<header class="sticky top-0 bg-trueGray-700" x-data="dropdown()">
     <div class="container flex items-center h-16 ">
-        <a href=""
-            class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
-            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round"
-                    stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        {{-- icono que contiene la palabra categorias --}}
+        <a x-on:click="show()"
+            class="flex flex-col items-center justify-center h-full px-4 font-semibold text-white bg-white bg-opacity-25 cursor-pointer">
+            <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16" />
             </svg>
             <span>Categorias</span>
         </a>
@@ -25,21 +16,21 @@
         {{-- los componentes ahora estana en, resources/views/vendor\jetstream --}}
 
         <a href="/" class="mx-6">
-            <x-jet-application-mark class="block h-9 w-auto" />
+            <x-jet-application-mark class="block w-auto h-9" />
 
         </a>
 
         @livewire('search')
 
         <!-- Settings Dropdown -->
-        <div class="mx-6 relative ">
+        <div class="relative mx-6 ">
             {{-- solo vamos a mostrar la imagen si es que estamos logueados --}}
             @auth
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                            class="flex text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                            <img class="object-cover w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}"
                                 alt="{{ Auth::user()->name }}" />
                         </button>
                     </x-slot>
@@ -70,7 +61,7 @@
                 <x-jet-dropdown align="right" width="48">
 
                     <x-slot name="trigger">
-                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                        <i class="text-3xl text-white cursor-pointer fas fa-user-circle"></i>
                     </x-slot>
 
                     <x-slot name="content">
@@ -91,15 +82,22 @@
         @livewire('dropdown-cart')
 
     </div>
+    {{-- la parte de :class..... es para que tome el valor de la variable open, porque no se abria el menu al poner la clase hidden --}}
+    <nav id="navigation-menu" 
+        x-on:click="show()"
 
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+
+        :class="{'block': open, 'hidden': !open}"
+        class="absolute w-full bg-opacity-25 bg-trueGray-700">
 
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            
+            {{-- el evento x-on-click.away es para ejecutar la accion cuando das click en cualquier parte menos en la del menu --}}
+            <div x-on:click.away="close()" class="relative grid h-full grid-cols-4" >
                 <ul class="bg-white">
                     @foreach ($categories as $category)
                         <li class=" navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
-                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                            <a href="" class="flex items-center px-4 py-2 text-sm">
                                 <span class="flex justify-center w-9">
                                     {{-- {!! xxxx !!} con  esto escapamos el codigo html, es decir para que nos imprima el codigo html y no lo imprima como texto plano --}}
                                     {!! $category->icon !!}
@@ -107,7 +105,7 @@
                                 {{ $category->name }}
                             </a>
 
-                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                            <div class="absolute top-0 right-0 hidden w-3/4 h-full bg-gray-100 navigation-submenu">
                                 {{-- llamamos al componente para mostrar category --}}
                                 <x-navigation-subcategories :category="$category" />
 
@@ -127,3 +125,4 @@
             </div>
     </nav>
 </header>
+
