3c8353480d0ef36d10a633e0114a5e2ee52a8675
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-04-05 23:36:47 +0200

    solo traer productos que esten publicados, osea 2, y solo muestro 15 productos por categoria

--- app/Http/Livewire/CategoryProducts.php
+++ app/Http/Livewire/CategoryProducts.php
@@ -12,7 +12,7 @@ class CategoryProducts extends Component
     public $products = [];
 
     function loadPosts(){
-        $this->products = $this->category->products;
+        $this->products = $this->category->products()->where('status', 2)->take(15)->get();
 
         $this->emit('glider', $this->category->id);
     }
